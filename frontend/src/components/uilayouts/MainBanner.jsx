86a8a6b07dae8190ceaa8ab52d6fd5d8713351ae
adiidubey <dubeyaditya397@gmail.com>
2025-08-25 22:31:59 +0530

    ui improvements

--- frontend/src/components/uilayouts/MainBanner.jsx
+++ frontend/src/components/uilayouts/MainBanner.jsx
@@ -18,7 +18,7 @@ const MainBanner = () => {
       />
 
       <div className="absolute inset-0 flex flex-col items-center md:items-start justify-end md:justify-center pb-24 md:pb-0 px-4 md:pl-18 lg:pl-24">
-        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-center md:text-left max-w-72 md:max-w-80 lg:max-w-105 leading-tight lg:leading-15">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-center md:text-left max-w-72 md:max-w-80 lg:max-w-105 leading-tight lg:leading-15 text-white">
           Freshness You Can Trust, Savings You Will Love!
         </h1>
         <div className="flex items-center mt-6 font-medium">
@@ -35,7 +35,7 @@ const MainBanner = () => {
           </Link>
           <Link
             to={"/products"}
-            className="group hidden md:flex items-center gap-2 px-9 py-3 cursor-pointer"
+            className="group hidden md:flex items-center gap-2 px-9 py-3 cursor-pointer text-white"
           >
             Explore Deals
             <img
