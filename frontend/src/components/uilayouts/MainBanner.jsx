1b6b8def4a7a39925f04c4ebaf66b6afba610fb8
adiidubey <dubeyaditya397@gmail.com>
2025-08-27 22:08:22 +0530

    ui improvements

--- frontend/src/components/uilayouts/MainBanner.jsx
+++ frontend/src/components/uilayouts/MainBanner.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-
 import { assets } from "../../assets/assets";
-import { Link } from "react-router-dom";
 
 const MainBanner = () => {
   return (
@@ -17,35 +15,7 @@ const MainBanner = () => {
         className="w-full md:hidden"
       />
 
-      <div className="absolute inset-0 flex flex-col items-center md:items-start justify-end md:justify-center pb-24 md:pb-0 px-4 md:pl-18 lg:pl-24">
-        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-center md:text-left max-w-72 md:max-w-80 lg:max-w-105 leading-tight lg:leading-15 text-white">
-          Freshness You Can Trust, Savings You Will Love!
-        </h1>
-        <div className="flex items-center mt-6 font-medium">
-          <Link
-            to={"/products"}
-            className="group flex items-center gap-2 px-7 md:px-9 py-3 bg-primary hover:bg-primary-dull transition rounded text-white cursor-pointer"
-          >
-            Shop Now
-            <img
-              className="md:hidden transition group-focus:translate-x-1"
-              src={assets.white_arrow_icon}
-              alt="arrow"
-            />
-          </Link>
-          <Link
-            to={"/products"}
-            className="group hidden md:flex items-center gap-2 px-9 py-3 cursor-pointer text-white"
-          >
-            Explore Deals
-            <img
-              className="transition group-hover:translate-x-1"
-              src={assets.black_arrow_icon}
-              alt="arrow"
-            />
-          </Link>
-        </div>
-      </div>
+      <div className="absolute inset-0 flex flex-col items-center md:items-start justify-end md:justify-center pb-24 md:pb-0 px-4 md:pl-18 lg:pl-24"></div>
     </div>
   );
 };
